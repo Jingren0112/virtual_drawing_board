be1483d0402a88c8b3978cfb21fc90174fd03597
Jingren Li <lij9@tcd.ie>
2021-05-07 03:26:15 +0100

    update hand mode
    fix some issue with drawing square in hand mode.

--- virtual_drawing_board.js
+++ virtual_drawing_board.js
@@ -2,7 +2,7 @@ let video;
 let handpose;                                                  //hand position detection array
 let hand;                                                      //result of detection
 let phand;                                                      //previous hand position
-var bgColor, shapeColor, shapeWidth,reset,handMode, mouseMode; //selections for the drawing board
+var bgColor, shapeColor, shapeWidth,reset,switchMode ; //selections for the drawing board
 var mode=true;                                                //switching mode
 var square, circle, triangle;                                            //select shape
 let sqr=false;
@@ -12,6 +12,9 @@ let hx=-1,hy=-1,phx=-1,phy=-1;        //variables for hand draw sqr
 let handFinished=true;
 let time=0;
 let pTime=0;
+let hint; //player instruction to draw square;
+let lineMode;          //line button
+let ln=true;        //line mode
 function setup() {
   createCanvas(1250, 480);
   video = createCapture(VIDEO);
@@ -23,6 +26,8 @@ function setup() {
   var optTitle = createDiv().parent(opts);
   createP('Color').parent(optTitle);
   createP('Width').parent(optTitle);
+  var hintDiv = createDiv().style('display:flex');
+  hint = createP('The default of the board is mouse mode drawing line').parent(hintDiv);
   var optValue = createDiv().parent(opts).style('margin: 20px 40px; width:50px');
   shapeColor = createColorPicker('#000000').parent(optValue);
   shapeWidth = createSelect(false).parent(optValue).style('margin-top:10px');
@@ -34,10 +39,10 @@ function setup() {
   shapeWidth.selected('2');
   var buttonOpt = createDiv().parent(opts).style('margin:10px 40px; width:75px');
   reset = createButton('reset').parent(buttonOpt).style('width:75px');
-  mouseMode = createButton('mouse mode').parent(buttonOpt).style('width:75px;margin-top:10px;');
-  handMode = createButton('hand mode').parent(buttonOpt).style('width:75px; margin-top:10px');
+  switchMode = createButton('hand mode').parent(buttonOpt).style('width:75px;margin-top:10px;');
   var buttonOpt2 = createDiv().parent(opts).style('margin:10px 40px; width:75px');
-  square = createButton('square').parent(buttonOpt2).style('width:75px');
+  lineMode = createButton('line').parent(buttonOpt2).style('width:75px');
+  square = createButton('square').parent(buttonOpt2).style('width:75px;margin-top:10px');
   background(255);
 }
 
@@ -56,15 +61,27 @@ function draw() {
     handFinished=true;
     pTime=0;
   });
-  mouseMode.mousePressed(function(){
-    mode=true;
+  switchMode.mousePressed(function(){
+    mode=!mode;
+    if(mode==true){
+      switchMode.html=('handMode',false);
+      hint.html('Now is mouseMode',false);
+    }else{
+      switchMode.html=('mouseMode',false);
+      hint.html('Now is handMode',false);
+    }
   });
-  handMode.mousePressed(function(){
-    mode=false;
+  lineMode.mousePressed(function(){
+    sqr=false;
+    ln=true;
+    hint.html('Now is drawing line',false);
   });
   square.mousePressed(function(){
     sqr=true;
+    ln=false;
+    hint.html('Now is drawing square',false);
   });
+  
   image(video,640,0,640,480);
   if(!mode){
     if(typeof hand!=='undefined'&&hand.length>0){
@@ -77,25 +94,24 @@ function draw() {
            pTime=time; 
           }
           if(sqr==true){
-            console.log(pTime);
-            console.log(time);
-            if(handFirstSqr==true&&handFinished==true){
-              console.log('first point!');
+            if(handFirstSqr==true&&handFinished==true&&(time-pTime)>1){
               drawSquare(X,Y,0);
               handFinished=false;
-            } else if(abs(pX-X)<10&&abs(pY-Y)<40&&handFinished==false&&(time-pTime)>3){
-              console.log('finished');
+              pTime=time;
+              hint.html("top vertex selected, move your finger now",false);
+           } else if((time-pTime)>1&&abs(pX-X)<10&&abs(pY-Y)<10){
                handFinished=true; 
+               handFirstSqr=true;
+               phx=phy=hx=hy=-1;
                pTime=time;
-            } else if(abs(pX-X)<10&&abs(pY-Y)>40&&handFinished==true&&handFirstSqr!=true){
-              console.log('started!');
-              handFinished=false;
-              handFirstSqr=true;
-            } else if(handFinished==false){
+              console.log('finished'); 
+              hint.html("draw finished, you have 1 second to move your finger to new location to draw new square",false);
+          }else if(abs(pX-X)>11&&abs(pY-Y)>11&&handFinished==false&&handFirstSqr!=true){
               console.log('drawing!');
                drawSquare(X,Y,1);
-            }
-          } else{
+               pTime=time;
+          } 
+          }else{
             stroke(shapeColor.value());
             strokeWeight(shapeWidth.value());
             line(phand[0].landmarks[8][0],phand[0].landmarks[8][1],hand[0].landmarks[8][0],hand[0].landmarks[8][1]);   
@@ -151,11 +167,17 @@ function mouseDragged(){
 
 function drawSquare(X,Y,z){
   if(z==0){
-    hx=hpx=X;
-    hy=hpy=Y;
+    console.log("X and Y are",X,Y);
+    hx=X;
+    hy=Y;
+    phx=hx;
+    phy=hy;
+    console.log("hx and hy are",hx,hy);
     handFirstSqr=false;
   } else{
-    if(X>phx&&Y>phy){
+    console.log("hx and hy is",hx,hy);
+    console.log("X and Y is",X,Y);
+    if(X>phx&&Y>phy&&phx!=-1&&phy!=-1&&hx!=-1&&hy!=-1&&X>hx&&Y>hy){
       stroke(shapeColor.value());
       fill(255);
       beginShape(QUADS);
@@ -166,9 +188,6 @@ function drawSquare(X,Y,z){
       endShape(CLOSE);
       phx=X;
       phy=Y;
-    }else if(X<hx||mouseY<hy){
-      phx=hx;
-      phy=hy;
     }
   }
 }
